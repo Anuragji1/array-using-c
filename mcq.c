ef6dc18fd5547407a532ab7a64143f43ddf53761
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-02-08 17:58:33 +0530

    Create mcq.c

---
+++ mcq.c
@@ -0,0 +1,28 @@
+**MCQ**
+
+MCQ - Statement) Multiple Choice Question
+You are given an array 
+�
+A of 
+�
+N integers.
+Find the maximum sum of two distinct integers in the array.
+
+Note: It is guaranteed that there exist at least two distinct integers in the array.
+
+What is the approach for this problem?
+
+Select one of the following options.
+
+Take the largest two elements and output their sum.
+
+Find the largest element. Then find the second largest element which is strictly smaller than the largest. Output the sum.
+
+Find the largest element. Then find the smallest element. Output the sum.
+
+A[N] + A[N-1]
+
+------------------------------------------------------------------------------
+SOLUTION = Correct Answer:
+
+Find the largest element. Then find the second largest element which is strictly smaller than the largest. Output the sum.
